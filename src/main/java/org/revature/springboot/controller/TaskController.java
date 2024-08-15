fc53906200c017f2193dacf609184001f891ae98
Shashankw11 <shashankwaghale@gmail.com>
2024-08-16 00:59:04 +0530

    Added the task functionality

---
+++ src/main/java/org/revature/springboot/controller/TaskController.java
@@ -0,0 +1,58 @@
+package org.revature.springboot.controller;
+
+import org.revature.springboot.model.Task;
+import org.revature.springboot.service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/tasks")
+public class TaskController {
+
+    private final TaskService taskService;
+
+    @Autowired
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Task>> getAllTasks() {
+        return new ResponseEntity<>(taskService.getAllTasks(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Task> getTaskById(@PathVariable Long id) {
+        return new ResponseEntity<>(taskService.getTaskById(id), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Task> createTask(@RequestBody Task task) {
+        return new ResponseEntity<>(taskService.createTask(task), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Task> updateTask(@PathVariable Long id, @RequestBody Task task) {
+        return new ResponseEntity<>(taskService.updateTask(task), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTask(@PathVariable Long id) {
+        taskService.deleteTask(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @GetMapping("/assignee/{assigneeId}")
+    public ResponseEntity<List<Task>> getTasksByAssigneeId(@PathVariable Long assigneeId) {
+        return new ResponseEntity<>(taskService.getTasksByAssigneeId(assigneeId), HttpStatus.OK);
+    }
+
+    @GetMapping("/project/{projectId}")
+    public ResponseEntity<List<Task>> getTasksByProjectId(@PathVariable Long projectId) {
+        return new ResponseEntity<>(taskService.getTasksByProjectId(projectId), HttpStatus.OK);
+    }
+}
