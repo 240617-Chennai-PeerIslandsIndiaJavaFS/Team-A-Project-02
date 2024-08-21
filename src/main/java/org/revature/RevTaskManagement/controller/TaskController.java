bc2b482694b63d79025144159c2ad0eb3819a81b
Shashankw11 <shashankwaghale@gmail.com>
2024-08-21 19:47:15 +0530

    Assign Task to Team Member Added

---
+++ src/main/java/org/revature/RevTaskManagement/controller/TaskController.java
@@ -0,0 +1,39 @@
+package org.revature.RevTaskManagement.controller;
+
+import org.revature.RevTaskManagement.models.Task;
+import org.revature.RevTaskManagement.service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+    @Autowired
+    private TaskService taskService;
+
+    @PostMapping("/create")
+    public ResponseEntity<Task> createTask(@RequestBody Task task) {
+        Task createdTask = taskService.createTask(task);
+        return ResponseEntity.ok(createdTask);
+    }
+
+    @GetMapping
+    public List<Task> getAllTasks() {
+        return taskService.getAllTasks();
+    }
+
+    @GetMapping("/by-username/{username}")
+    public List<Task> getTasksByUsername(@PathVariable String username) {
+        return taskService.getTasksByUsername(username);
+    }
+
+    @PutMapping("/update-milestone")
+    public Task updateTaskMilestone(
+            @RequestParam int taskId,
+            @RequestParam int milestoneId) {
+        return taskService.updateTaskMilestone(taskId, milestoneId);
+    }
+}
